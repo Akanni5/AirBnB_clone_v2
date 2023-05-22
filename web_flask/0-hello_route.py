a34c6cd2ac1623f73073bcdd9e0d64527d5ce30e
Akanni Emmanuel <akannie33@gmail.com>
2023-05-22 11:02:17 +0100

    fixed minor issue

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,7 @@
-"""
-script that creates a flask app
-and says "Hello HBNB" at /
-"""
-from flask import Flask
+#!usr/bin/python3
+"""script that creates a flask app and says 'Hello HBNB' """
 
+from flask import Flask
 
 app = Flask(__name__)
 
