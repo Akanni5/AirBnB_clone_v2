e5c90a69f18a99f98500b6957eb3ac901926ba6f
Akanni Emmanuel <akannie33@gmail.com>
2023-05-22 11:33:19 +0100

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""script that creates a flask app and says 'Hello HBNB' """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ handler for default route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """ handle hbnb route """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ displays C follow by the value of @text """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', defaults={"text": "is_cool"} , strict_slashes=False)
+def python_route(text):
+    """ displays C along with the value of @text """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
