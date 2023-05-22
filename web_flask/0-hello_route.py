fadd05627190e03da665d2a7e4f035b27fb0668b
Akanni Emmanuel <akannie33@gmail.com>
2023-05-22 10:55:59 +0100

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+"""
+script that creates a flask app
+and says "Hello HBNB" at /
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ handler for / route """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
