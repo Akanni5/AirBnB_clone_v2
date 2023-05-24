6cb85cc28200502e3414a3b4e3c40e893f0d3117
Akanni Emmanuel <akannie33@gmail.com>
2023-05-24 17:59:55 +0100

    list states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""list of states"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """closes the storage when app context ends"""
+    storage.close()
+
+
+@app.route('/state_list', strict_slashes=False)
+def state_list():
+    """display a HTML page with the list of states
+    present in the DB.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
